de3b35a8a6417df4baa97e3708a8848e7af1beec
cpheighter <cheighterjr@gmail.com>
2022-07-05 12:46:20 -0400

    1st init

---
+++ MethodExercise/MathMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise
+{
+    internal class MathMethod
+    {
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        //-----------------------------------------------------------
+        public static double AddAnything(params double[] allnum)
+        {
+            double total = 0;
+
+            foreach (double num in allnum)
+            {
+                total += num;
+            }
+            return total;
+        }
+    }
+}
---
+++ MethodExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("What is your age?");
+string age = Console.ReadLine();
+Console.WriteLine("Enter a color");
+string color1 = Console.ReadLine();
+Console.WriteLine("Enter a 2nd color");
+string color2 = Console.ReadLine();
+Console.WriteLine("What is your favorite artist?");
+string artist = Console.ReadLine();
+
+Console.WriteLine("There once was a man named " + name + ". He was " + age + " years old. He really liked the name " + name + ". But didnt like being " + age);
+Console.WriteLine(name + " made a poem to his favorite band:");
+Console.WriteLine("Roses are " + color1);
+Console.WriteLine("Violets are " + color2);
+Console.WriteLine("I Love " + artist + ". How about you?");
+
